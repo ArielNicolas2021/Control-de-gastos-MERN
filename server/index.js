8fb3e7d11580d466a7626f1ad25b2f5dbd071c6f
Ariel <ariel.nicolas.aguilar@gmail.com>
2025-09-05 13:35:19 -0300

    fix cors

--- server/index.js
+++ server/index.js
@@ -10,12 +10,8 @@ import cors from "cors"
 // Global configs
 const app = express()
 app.use(express.json())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // O tu origen específico
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+app.use(cors())
+
 const PORT = 1234
 app.use('/api/items', auth, Enrouter(Item_MDB))
 app.use('/api/users', CreateUsers(User_MDB))
